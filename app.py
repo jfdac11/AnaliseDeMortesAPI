d0af71f3eeca3bfff1b363346c3e192b134e12ed
Davi Costa <davi.machado.costa@hotmail.com>
2021-11-07 16:54:47 -0300

    Conectando ao banco através do environment

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from pydantic import BaseModel, Field, EmailStr
 from bson import ObjectId
 from typing import Optional, List
 import motor.motor_asyncio
+from dotenv import load_dotenv
+
+load_dotenv('./.env')
 
 app = FastAPI()
 client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
