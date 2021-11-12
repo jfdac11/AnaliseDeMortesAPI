f4a47fce7ca59861d5d00cbbe75850af41334f33
Maria Antônia Amado <48838678+AmadoMaria@users.noreply.github.com>
2021-11-12 14:57:39 -0300

    Correção CORSMiddleware

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from typing import List
 from modelos.declaracao_obito import DeclaracaoObitoModel
 from servicos.declaracao_obito.index import ServicoDeclaracaoObito
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get(
