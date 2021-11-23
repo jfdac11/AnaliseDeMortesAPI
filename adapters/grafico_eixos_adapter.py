307c8e8aa302d4d3862049b1f38d15148eaf423e
Davi Costa <davi.machado.costa@hotmail.com>
2021-11-23 17:57:17 -0300

    Retirando ponto e virgula desnecessário

--- adapters/grafico_eixos_adapter.py
+++ adapters/grafico_eixos_adapter.py
@@ -1,5 +1,6 @@
 from adapters.listar_declaracoes_ano import ListarDeclaracoes
 
+
 class GraficoEixosAdapter(ListarDeclaracoes):
 
     def gerar_eixo(self, lista, chave):
@@ -11,7 +12,7 @@ class GraficoEixosAdapter(ListarDeclaracoes):
     async def gerar_grafico(self, ano: str, match: object = None):
         eixos = []
         res_aggregate = await self.aggregate(ano, match)
-        chaves = res_aggregate[0].keys();
+        chaves = res_aggregate[0].keys()
         for chave in chaves:
             eixo = self.gerar_eixo(res_aggregate, chave)
             eixos.append(eixo)
