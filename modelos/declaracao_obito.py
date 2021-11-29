36622153a8c62dccf1789bfd8d81b7247b9e768e
Davi Costa <davi.machado.costa@hotmail.com>
2021-11-28 21:35:46 -0300

    Alterando o modelo de dados

--- modelos/declaracao_obito.py
+++ modelos/declaracao_obito.py
@@ -6,37 +6,18 @@ from modelos.py_object_id import PyObjectId
 
 class DeclaracaoObitoModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    SEXO: Optional[str] = Field(None)
-    RACACOR: Optional[str] = Field(None)
-    ESC2010: Optional[str] = Field(None)
-    LOCOCOR: Optional[str] = Field(None)
-    CODMUNOCOR: Optional[str] = Field(None)
-    DIA_OBITO: Optional[str] = Field(None)
-    MES_OBITO: Optional[str] = Field(None)
-    ANO_OBITO: Optional[str] = Field(None)
-    ESTADO: Optional[str] = Field(None)
-    GRUPO_CAUSA_BASICA: Optional[str] = Field(None)
-    CAUSA_BASICA: Optional[str] = Field(None)
-    OCUPACAO: Optional[str] = Field(None)
-    GRUPO_OCUPACAO: Optional[str] = Field(None)
+    idade: Optional[int] = Field(None)
+    sexo: Optional[str] = Field(None)
+    mes_obito: Optional[str] = Field(None)
+    raca_cor: Optional[str] = Field(None)
+    escolaridade: Optional[str] = Field(None)
+    estado: Optional[str] = Field(None)
+    codigo_estado: Optional[str] = Field(None)
+    cod_grupo_causa_basica: Optional[str] = Field(None)
+    cod_causa_basica: Optional[str] = Field(None)
+    cod_capitulo_causa_basica: Optional[str] = Field(None)
+    codigo_grupo_ocupacao: Optional[str] = Field(None)
 
     class Config:
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
-
-# class DeclaracaoObitoModel(BaseModel):
-#     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-#     idade: Optional[str] = Field(None)
-#     sexo: Optional[str] = Field(None)
-#     raca_cor: Optional[str] = Field(None)
-#     escolaridade: Optional[str] = Field(None)
-#     local_ocorrencia: Optional[str] = Field(None)
-#     teve_assistencia_medica: Optional[str] = Field(None)
-#     dia_obito: Optional[str] = Field(None)
-#     mes_obito: Optional[str] = Field(None)
-#     ano_obito: Optional[str] = Field(None)
-#     estado: Optional[str] = Field(None)
-#     grupo_causa_basica: Optional[str] = Field(None)
-#     causa_basica: Optional[str] = Field(None)
-#     ocupacao: Optional[str] = Field(None)
-#     grupo_ocupacao: Optional[str] = Field(None)
